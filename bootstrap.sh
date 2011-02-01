b0de4f0c58b321829ad77122d24186656de27378
Arnold D. Robbins <arnold@skeeve.com>
2011-02-01 23:14:13 +0200

    Fix bootstrap.sh.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 # bootstrap.sh --- touch relevant files to avoid out-of-date issues in
 #		   Git sandboxes
 
-# Copyright (C) 2007, 2009, 2010 the Free Software Foundation, Inc.
+# Copyright (C) 2007, 2009, 2010, 2011 the Free Software Foundation, Inc.
 # 
 # This file is part of GAWK, the GNU implementation of the
 # AWK Programming Language.
@@ -22,7 +22,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
-find . -name aclocal.m4 -print | xargs touch
+touch aclocal.m4
 find awklib -type f -print | xargs touch
 sleep 1
 touch configure
