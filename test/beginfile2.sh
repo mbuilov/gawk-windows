9773d7150bc164f72806b2b31fc5f43a4a115721
Arnold D. Robbins <arnold@skeeve.com>
2011-10-24 05:25:30 +0200

    Portability fix in test/beginfile2.sh.

--- test/beginfile2.sh
+++ test/beginfile2.sh
@@ -13,70 +13,70 @@ then
 fi
 
 echo "--Test 1a--"
-prog=$($AWK '/#TEST1#/, /#TEST2#/' $AWKPROG)
+prog=`$AWK '/#TEST1#/, /#TEST2#/' $AWKPROG`
 $AWK "$prog" $AWKPROG
 echo "--Test 1b--"
 $AWK "$prog" $AWKPROG /file/does/not/exist
 
 echo "--Test 2--"
-prog=$($AWK '/#TEST2#/, /#TEST3#/' $AWKPROG)
+prog=`$AWK '/#TEST2#/, /#TEST3#/' $AWKPROG`
 $AWK "$prog" $AWKPROG /file/does/not/exist
 
 echo "--Test 3--"
-prog=$($AWK '/#TEST3#/, /#TEST4#/' $AWKPROG)
+prog=`$AWK '/#TEST3#/, /#TEST4#/' $AWKPROG`
 $AWK -vsrc=$SCRIPT "$prog" $AWKPROG
 
 echo "--Test 4--"
-prog=$($AWK '/#TEST4#/, /#TEST5#/' $AWKPROG)
+prog=`$AWK '/#TEST4#/, /#TEST5#/' $AWKPROG`
 $AWK -vsrc=$SCRIPT "$prog" $AWKPROG
 
 echo "--Test 5--"
-prog=$($AWK '/#TEST5#/, /#TEST6#/' $AWKPROG)
+prog=`$AWK '/#TEST5#/, /#TEST6#/' $AWKPROG`
 $AWK "$prog" $AWKPROG
 
 echo "--Test 6--"
-prog=$($AWK '/#TEST6#/, /#TEST7#/' $AWKPROG)
+prog=`$AWK '/#TEST6#/, /#TEST7#/' $AWKPROG`
 $AWK "$prog" $AWKPROG
 
 echo "--Test 7--"
-prog=$($AWK '/#TEST7#/, /#TEST8#/' $AWKPROG)
+prog=`$AWK '/#TEST7#/, /#TEST8#/' $AWKPROG`
 $AWK "$prog" $AWKPROG $SCRIPT
 
 echo "--Test 8--"
-prog=$($AWK '/#TEST8#/, /#TEST9#/' $AWKPROG)
+prog=`$AWK '/#TEST8#/, /#TEST9#/' $AWKPROG`
 $AWK "$prog" $AWKPROG
 
 echo "--Test 9a--"
-prog=$($AWK '/#TEST9#/, /#TEST10#/' $AWKPROG)
+prog=`$AWK '/#TEST9#/, /#TEST10#/' $AWKPROG`
 $AWK "$prog" /file/does/not/exist $AWKPROG
 echo "--Test 9b--"
 $AWK -vskip=1 "$prog" /file/does/not/exist $AWKPROG
 
 echo "--Test 10--"
-prog=$($AWK '/#TEST10#/, /#TEST11#/' $AWKPROG)
+prog=`$AWK '/#TEST10#/, /#TEST11#/' $AWKPROG`
 $AWK "$prog" $AWKPROG $SCRIPT
 
 echo "--Test 11--"
-prog=$($AWK '/#TEST11#/, /#TEST12#/' $AWKPROG)
+prog=`$AWK '/#TEST11#/, /#TEST12#/' $AWKPROG`
 $AWK "$prog" $AWKPROG $SCRIPT
 
 echo "--Test 12--"
-prog=$($AWK '/#TEST12#/, /#TEST13#/' $AWKPROG)
+prog=`$AWK '/#TEST12#/, /#TEST13#/' $AWKPROG`
 $AWK "$prog" $AWKPROG $SCRIPT
 
 echo "--Test 13--"
-prog=$($AWK '/#TEST13#/, /#TEST14#/' $AWKPROG)
+prog=`$AWK '/#TEST13#/, /#TEST14#/' $AWKPROG`
 $AWK "$prog" $AWKPROG $SCRIPT
 
 echo "--Test 14--"
-prog=$($AWK '/#TEST14#/, /#TEST15#/' $AWKPROG)
+prog=`$AWK '/#TEST14#/, /#TEST15#/' $AWKPROG`
 $AWK "$prog" $AWKPROG $SCRIPT
 
 echo "--Test 15--"
-prog=$($AWK '/#TEST15#/, /#TEST16#/' $AWKPROG)
+prog=`$AWK '/#TEST15#/, /#TEST16#/' $AWKPROG`
 $AWK "$prog" $AWKPROG $SCRIPT
 
 echo "--Test 16--"
-prog=$($AWK '/#TEST16#/, /#TEST17#/' $AWKPROG)
+prog=`$AWK '/#TEST16#/, /#TEST17#/' $AWKPROG`
 $AWK "$prog" $AWKPROG $SCRIPT
 
