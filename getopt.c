089476b5415e9844175a2fbf1bde6faf18ae0b61
Arnold D. Robbins <arnold@skeeve.com>
2012-03-28 21:45:38 +0200

    Additional getopt fix, for DJGPP this time.

--- getopt.c
+++ getopt.c
@@ -58,9 +58,12 @@
 
 /* This needs to come after some library #include
    to get __GNU_LIBRARY__ defined.  */
-#if defined (__GNU_LIBRARY__) || defined (__CYGWIN__)
-/* Don't include stdlib.h for non-GNU C libraries and non-Cygwin because some
-   of them contain conflicting prototypes for getopt.  */
+#if defined (__GNU_LIBRARY__) || defined (__CYGWIN__) || defined(DJGPP)
+/* Don't include stdlib.h for
+ * non-GNU C libraries
+ * non-Cygwin
+ * non-DJGPP
+ * because some of them contain conflicting prototypes for getopt.  */
 # include <stdlib.h>
 # include <unistd.h>
 #endif	/* GNU C library.  */
