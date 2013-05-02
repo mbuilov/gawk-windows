e5e1b44b6e5a0c2d132587db9cefcfc6f3403a26
Arnold D. Robbins <arnold@skeeve.com>
2013-05-02 22:55:48 +0300

    Update for next test tarball.

--- pc/config.h
+++ pc/config.h
@@ -424,7 +424,7 @@
 #define PACKAGE_NAME "GNU Awk"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GNU Awk 4.0.76"
+#define PACKAGE_STRING "GNU Awk 4.0.77"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gawk"
@@ -433,7 +433,7 @@
 #define PACKAGE_URL "http://www.gnu.org/software/gawk/"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "4.0.76"
+#define PACKAGE_VERSION "4.0.77"
 
 /* Define to 1 if *printf supports %F format */
 #undef PRINTF_HAS_F_FORMAT
@@ -495,7 +495,7 @@
 
 
 /* Version number of package */
-#define VERSION "4.0.76"
+#define VERSION "4.0.77"
 
 /* Enable large inode numbers on Mac OS X 10.5.  */
 #ifndef _DARWIN_USE_64_BIT_INODE
