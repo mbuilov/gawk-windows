772793d99fbb08a03576c375b870ec36bb11cb8d
Arnold D. Robbins <arnold@skeeve.com>
2013-12-24 21:42:53 +0200

    Fix getopt.c for Illumos.

--- getopt.c
+++ getopt.c
@@ -57,7 +57,7 @@
 
 /* This needs to come after some library #include
    to get __GNU_LIBRARY__ defined.  */
-#if defined (__GNU_LIBRARY__) || defined (__CYGWIN__) || defined(__DJGPP__) || defined(__APPLE__) || defined(__MINGW32__)
+#if defined (__GNU_LIBRARY__) || defined (__CYGWIN__) || defined(__DJGPP__) || defined(__APPLE__) || defined(__MINGW32__) || defined(__sun) /* Illumos */
 /* Don't include stdlib.h for
  * non-GNU C libraries
  * non-Cygwin
