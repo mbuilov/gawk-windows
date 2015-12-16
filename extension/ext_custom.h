f783601d333d7875e790e7e755a0b24e1110533d
Arnold D. Robbins <arnold@skeeve.com>
2015-12-16 05:53:15 +0200

    Autoconf fixups in extension directory.

---
+++ extension/ext_custom.h
@@ -0,0 +1,37 @@
+/*
+ * ext_custom.h
+ *
+ * This file is for use on systems where Autoconf isn't quite able to
+ * get things right. It is appended to the bottom of config.h by configure,
+ * in order to override definitions from Autoconf that are erroneous. See
+ * the manual for more information.
+ *
+ * If you make additions to this file for your system, please send me
+ * the information, to arnold@skeeve.com.
+ */
+
+/* 
+ * Copyright (C) 2015 the Free Software Foundation, Inc.
+ * 
+ * This file is part of GAWK, the GNU implementation of the
+ * AWK Programming Language.
+ * 
+ * GAWK is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * GAWK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
+/* From Michal Jaegermann for bleeding edge GLIBC. */
+#if defined _GNU_SOURCE && !defined _DEFAULT_SOURCE
+# define _DEFAULT_SOURCE
+#endif
