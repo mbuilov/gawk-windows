b54c54d30fe20cfb5f0794cd85eeec7783800866
Andrew J. Schorr <aschorr@telemetry-investments.com>
2016-06-13 22:57:15 -0400

    Eliminate more STRCUR tests in awkgram.y

--- awkgram.c
+++ awkgram.c
@@ -3822,7 +3822,7 @@ regular_print:
 					&& ((yyvsp[0])->nexti->memory->flags & (MPFN|MPZN)) == 0
 			) {
 				NODE *n = (yyvsp[0])->nexti->memory;
-				if ((n->flags & (STRCUR|STRING)) != 0) {
+				if ((n->flags & STRING) != 0) {
 					n->numbr = (AWKNUM) (n->stlen == 0);
 					n->flags &= ~(STRCUR|STRING);
 					n->flags |= (NUMCUR|NUMBER);
@@ -3924,7 +3924,7 @@ regular_print:
 #line 1791 "awkgram.y" /* yacc.c:1646  */
     {
 		if ((yyvsp[0])->lasti->opcode == Op_push_i
-			&& ((yyvsp[0])->lasti->memory->flags & (STRCUR|STRING)) == 0
+			&& ((yyvsp[0])->lasti->memory->flags & STRING) == 0
 		) {
 			NODE *n = (yyvsp[0])->lasti->memory;
 			(void) force_number(n);
@@ -6908,7 +6908,7 @@ snode(INSTRUCTION *subn, INSTRUCTION *r)
 	} else if (do_intl					/* --gen-po */
 			&& r->builtin == do_dcgettext		/* dcgettext(...) */
 			&& subn->nexti->lasti->opcode == Op_push_i	/* 1st arg is constant */
-			&& (subn->nexti->lasti->memory->flags & STRCUR) != 0) {	/* it's a string constant */
+			&& (subn->nexti->lasti->memory->flags & STRING) != 0) {	/* it's a string constant */
 		/* ala xgettext, dcgettext("some string" ...) dumps the string */
 		NODE *str = subn->nexti->lasti->memory;
 
@@ -6920,9 +6920,9 @@ snode(INSTRUCTION *subn, INSTRUCTION *r)
 	} else if (do_intl					/* --gen-po */
 			&& r->builtin == do_dcngettext		/* dcngettext(...) */
 			&& subn->nexti->lasti->opcode == Op_push_i	/* 1st arg is constant */
-			&& (subn->nexti->lasti->memory->flags & STRCUR) != 0	/* it's a string constant */
+			&& (subn->nexti->lasti->memory->flags & STRING) != 0	/* it's a string constant */
 			&& subn->nexti->lasti->nexti->lasti->opcode == Op_push_i	/* 2nd arg is constant too */
-			&& (subn->nexti->lasti->nexti->lasti->memory->flags & STRCUR) != 0) {	/* it's a string constant */
+			&& (subn->nexti->lasti->nexti->lasti->memory->flags & STRING) != 0) {	/* it's a string constant */
 		/* ala xgettext, dcngettext("some string", "some plural" ...) dumps the string */
 		NODE *str1 = subn->nexti->lasti->memory;
 		NODE *str2 = subn->nexti->lasti->nexti->lasti->memory;
