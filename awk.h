5557a45fbe813ba13eda8b78a16f84621a9794ee
Arnold D. Robbins <arnold@skeeve.com>
2017-08-01 22:58:00 +0300

    Initial additions to update DJGPP port.

--- awk.h
+++ awk.h
@@ -182,9 +182,9 @@ extern void *memset_ulong(void *dest, int val, unsigned long l);
 #define fwrite	fwrite_unlocked
 #endif /* HAVE_FWRITE_UNLOCKED */
 
-#if defined(__EMX__) || defined(__MINGW32__)
+#if defined(__DJGPP__) || defined(__EMX__) || defined(__MINGW32__)
 #include "nonposix.h"
-#endif /* defined(__EMX__) || defined(__MINGW32__) */
+#endif /* defined(__DJGPP__) || defined(__EMX__) || defined(__MINGW32__) */
 
 /* use this as lintwarn("...")
    this is a hack but it gives us the right semantics */
