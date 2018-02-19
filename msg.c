a7db2037c7e6ccd7ea4a10e01f7455f79c4d6b67
Arnold D. Robbins <arnold@skeeve.com>
2018-02-19 19:52:43 +0200

    Update copyright years.

--- msg.c
+++ msg.c
@@ -3,7 +3,7 @@
  */
 
 /*
- * Copyright (C) 1986, 1988, 1989, 1991-2001, 2003, 2010-2013, 2017,
+ * Copyright (C) 1986, 1988, 1989, 1991-2001, 2003, 2010-2013, 2017, 2018,
  * the Free Software Foundation, Inc.
  *
  * This file is part of GAWK, the GNU implementation of the
