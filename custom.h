d142c8609ceca2d4e2c026d24c3d7b51215a7db9
Arnold D. Robbins <arnold@skeeve.com>
2019-01-28 21:01:56 +0200

    Update copyright year in many dates.

--- custom.h
+++ custom.h
@@ -11,7 +11,7 @@
  */
 
 /*
- * Copyright (C) 1995-2004, 2008, 2009, 2011, 2016
+ * Copyright (C) 1995-2004, 2008, 2009, 2011, 2016, 2018
  * the Free Software Foundation, Inc.
  *
  * This file is part of GAWK, the GNU implementation of the
