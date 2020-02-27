dd151a65d803789cca32f4e9cfb290263f1d4279
Michael Builov <mbuilov@gmail.com>
2020-02-27 12:04:25 +0300

    split-out __MINGW32__ && _MSC_VER code

--- pc/popen.c
+++ pc/popen.c
@@ -9,6 +9,129 @@
 #undef pclose
 #undef system
 
+#if defined(__MINGW32__) || defined(_MSC_VER)
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#ifdef _MSC_VER
+#define popen(cmd, mode) _popen(cmd, mode)
+#define pclose(file)     _pclose(file)
+#endif
+
+int
+os_system(const char *cmd)
+{
+  const char *cmdexe = getenv("ComSpec");
+  char *cmd1 = quote_cmd(cmd);
+
+  /* It's safe to truncate 64-bit process handle
+    to 32-bit integer on 64-bit Windows.  */
+  int pid = (int) spawnl(P_WAIT, cmdexe, "cmd.exe", "/D /S /C", cmd1, NULL);
+
+  free(cmd1);
+  return pid;
+}
+
+FILE *
+os_popen(const char *command, const char *mode)
+{
+  FILE *current;
+
+  if (*mode != 'r' && *mode != 'w')
+    return NULL;
+
+  current = popen(command, mode);
+  return current;
+}
+
+int
+os_pclose(FILE *current)
+{
+  int rval;
+
+  rval = pclose(current);
+  return rval;
+}
+
+int
+kill (int pid, int sig)
+{
+  HANDLE ph;
+  int retval = 0;
+
+  /* We only support SIGKILL.  */
+  if (sig != SIGKILL)
+    {
+      errno = ENOSYS;
+      return -1;
+    }
+
+  ph = OpenProcess(PROCESS_TERMINATE, FALSE, (DWORD) pid);
+  if (ph)
+    {
+      BOOL status = TerminateProcess(ph, (UINT) -1);
+
+      if (!status)
+        {
+          errno = EPERM;
+          retval = -1;
+        }
+      CloseHandle (ph);
+    }
+  else
+    {
+      /* If we cannot open the process, it means we eaither aren't
+         allowed to (e.g., a process of another user), or such a
+         process doesn't exist.  */
+      switch (GetLastError ())
+        {
+          case ERROR_ACCESS_DENIED:
+            errno = EPERM;
+            break;
+          default:
+            errno = ESRCH;
+            break;
+        }
+      retval = -1;
+    }
+  return retval;
+}
+
+char *
+quote_cmd(const char *cmd)
+{
+  char *quoted;
+
+  /* The command will be invoked via cmd.exe, whose behavior wrt
+     quoted commands is to remove the first and the last quote
+     characters, and leave the rest (including any quote characters
+     inside the outer pair) intact.  */
+
+  /* M.B. Note: if cmd.exe is run without "/S" switch, then:
+    spawnl(P_WAIT, cmdexe, "cmd.exe", "/C", quote_cmd("c:\\x y\\foo"), NULL);
+   will result in calling
+    cmd /c "c:\x y\foo"
+   which in turn will try to run "c:\x y\foo.exe" (instead of "c:\x.exe"
+    - it will be called only if "c:\x y\foo.exe" does not exist).
+   This oddly behaviour can be avoided by the "/S" switch:
+    spawnl(P_WAIT, cmdexe, "cmd.exe", "/S /C", quote_cmd("c:\\x y\\foo"), NULL);
+   will result in calling
+    cmd /s /c "c:\x y\foo"
+   which in turn will run "c:\x.exe" with "y\foo" as the argument.
+   So, if the path to executable may contain spaces, put the path into
+    double-quotes (like under Unix, e.g.: /bin/sh -c '"/my executable"').
+   See https://stackoverflow.com/questions/9866962/what-is-cmd-s-for */
+
+  quoted = (char*)malloc(strlen(cmd) + 2 + 1);
+  if (quoted)
+    sprintf(quoted, "\"%s\"", cmd);
+
+  return quoted;
+}
+
+#else /* !__MINGW32__ && !_MSC_VER */
+
 #ifndef _NFILE
 #define _NFILE 40
 #endif
@@ -19,7 +142,6 @@ static struct {
   char pmode[4];
 } pipes[_NFILE];
 
-
 /*
  * For systems where system() and popen() do not follow SHELL:
  *  1. Write command to temp file.  Temp filename must have slashes
@@ -27,11 +149,6 @@ static struct {
  *  2. Convert slashes in SHELL (if present) to be compatible with COMSPEC.
  * Currently, only MSC (running under DOS) and MINGW versions are managed.
  */
-
-#if defined(__MINGW32__)
-
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
 
 #if 0
 static int
@@ -84,8 +201,8 @@ scriptify(const char *command)
   if (! (i = unixshell(p))) {
     char *p = (char *) realloc(name, strlen(name) + 5);
     if (p == NULL) {
-	free(cmd);
-	return NULL;
+        free(cmd);
+        return NULL;
     }
     name = p;
     strcat(name, ".bat");
@@ -121,82 +238,7 @@ unlink_and_free(char *cmd)
 }
 #endif
 
-int
-os_system(const char *cmd)
-{
-  char *cmdexe = getenv("ComSpec");
-  char *cmd1 = quote_cmd(cmd);
-  int i = spawnl(P_WAIT, cmdexe, "cmd.exe", "/c", cmd1, NULL);
-
-  free(cmd1);
-  return(i);
-}
-
-#ifndef PIPES_SIMULATED
-int
-kill (int pid, int sig)
-{
-  HANDLE ph;
-  int retval = 0;
-
-  /* We only support SIGKILL.  */
-  if (sig != SIGKILL)
-    {
-      errno = ENOSYS;
-      return -1;
-    }
-
-  ph = OpenProcess(PROCESS_TERMINATE, FALSE, pid);
-  if (ph)
-    {
-      BOOL status = TerminateProcess(ph, -1);
-
-      if (!status)
-	{
-	  errno = EPERM;
-	  retval = -1;
-	}
-    }
-  else
-    {
-      /* If we cannot open the process, it means we eaither aren't
-	 allowed to (e.g., a process of another user), or such a
-	 process doesn't exist.  */
-      switch (GetLastError ())
-	{
-	  case ERROR_ACCESS_DENIED:
-	    errno = EPERM;
-	    break;
-	  default:
-	    errno = ESRCH;
-	    break;
-	}
-      retval = -1;
-    }
-  CloseHandle (ph);
-  return retval;
-}
-
-char *
-quote_cmd(const char *cmd)
-{
-  char *quoted;
-
-  /* The command will be invoked via cmd.exe, whose behavior wrt
-     quoted commands is to remove the first and the last quote
-     characters, and leave the rest (including any quote characters
-     inside the outer pair) intact.  */
-  quoted = malloc(strlen (cmd) + 2 + 1);
-  sprintf(quoted, "\"%s\"", cmd);
-
-  return quoted;
-}
-#endif
-
-#else  /* !__MINGW32__ */
 #define os_system(cmd) system(cmd)
-#endif
-
 
 FILE *
 os_popen(const char *command, const char *mode )
@@ -210,13 +252,6 @@ os_popen(const char *command, const char *mode )
       return NULL;
     strncpy(curmode, mode, 3); curmode[3] = '\0';
 
-#if defined(__MINGW32__)
-    current = popen(command, mode);
-    cur = fileno(current);
-    strcpy(pipes[cur].pmode, curmode);
-    return(current);
-#endif
-
     /*
     ** get a name to use.
     */
@@ -227,17 +262,16 @@ os_popen(const char *command, const char *mode )
     ** output.
     */
     if (*curmode == 'r') {
-        FILE *fp;
         if ((cur = dup(fileno(stdout))) == -1)
-	    return NULL;
-	*curmode = 'w';
+            return NULL;
+        *curmode = 'w';
         if ((current = freopen(name, curmode, stdout)) == NULL)
-	    return NULL;
+            return NULL;
         os_system(command);
         if (dup2(cur, fileno(stdout)) == -1)
-	    return NULL;
-	close(cur);
-	*curmode = 'r';
+            return NULL;
+        close(cur);
+        *curmode = 'r';
         if ((current = fopen(name, curmode)) == NULL)
             return NULL;
     } else {
@@ -257,12 +291,6 @@ os_pclose( FILE * current)
     int cur = fileno(current);
     int fd, rval;
 
-#if defined(__MINGW32__)
-    rval = pclose(current);
-    *pipes[cur].pmode = '\0';
-    return rval;
-#endif
-
     /*
     ** check for an open file.
     */
@@ -273,25 +301,25 @@ os_pclose( FILE * current)
         */
         rval = fclose(current);
         unlink(pipes[cur].name);
-	break;
+        break;
     case 'w':
         /*
         ** output pipes are temporary files we have
         ** to cram down the throats of programs.
         */
         fclose(current);
-	rval = -1;
-	if ((fd = dup(fileno(stdin))) != -1) {
-	  char *mode = pipes[cur].pmode; *mode = 'r';
-	  if (current = freopen(pipes[cur].name, mode, stdin)) {
-	    rval = os_system(pipes[cur].command);
-	    fclose(current);
-	    if (dup2(fd, fileno(stdin)) == -1) rval = -1;
-	    close(fd);
-	  }
-	}
+        rval = -1;
+        if ((fd = dup(fileno(stdin))) != -1) {
+          char *mode = pipes[cur].pmode; *mode = 'r';
+          if (current = freopen(pipes[cur].name, mode, stdin)) {
+            rval = os_system(pipes[cur].command);
+            fclose(current);
+            if (dup2(fd, fileno(stdin)) == -1) rval = -1;
+            close(fd);
+          }
+        }
         unlink(pipes[cur].name);
-	break;
+        break;
     default:
       return -1;
     }
@@ -303,3 +331,5 @@ os_pclose( FILE * current)
     free(pipes[cur].command);
     return rval;
 }
+
+#endif /* !__MINGW32__ && !_MSC_VER */
