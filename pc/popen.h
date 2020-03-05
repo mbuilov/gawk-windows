5da53167ba14749e185fda15a3c2c322792451c6
Michael Builov <mbuilov@gmail.com>
2020-03-05 10:34:56 +0300

    port to _MSC_VER

--- pc/popen.h
+++ pc/popen.h
@@ -10,11 +10,11 @@
 # define pclose(f)	os_pclose(f)
   extern FILE *os_popen( const char *, const char * );
   extern int  os_pclose( FILE * );
-# ifdef __MINGW32__
+# if defined __MINGW32__ || defined _MSC_VER
 #  define system(c)	os_system(c)
    extern int os_system( const char * );
 #  define SIGKILL	9
    extern int kill( int, int );
    extern char *quote_cmd( const char * );
-# endif	 /* __MINGW32__ */
+# endif	 /* __MINGW32__ || _MSC_VER */
 #endif	/* !__DJGPP__ */
