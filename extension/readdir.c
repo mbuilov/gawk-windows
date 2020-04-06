ffc5185b001013a8640dd07893a7eb444d9c3f56
Michael M. Builov <mbuilov@gmail.com>
2020-04-06 20:07:50 +0300

    reorder included headers: api pointer may be defined later

--- extension/readdir.c
+++ extension/readdir.c
@@ -69,6 +69,16 @@
 
 #include "gawkapi.h"
 
+#include "gawkdirfd.h"
+
+#include "gettext.h"
+#define _(msgid)  gettext(msgid)
+#define N_(msgid) msgid
+
+#ifndef PATH_MAX
+#define PATH_MAX	1024	/* a good guess */
+#endif
+
 GAWK_PLUGIN_GPL_COMPATIBLE
 
 static const gawk_api_t *api;	/* for convenience macros to work */
@@ -78,16 +88,6 @@ static awk_ext_id_t ext_id;
 static const char *ext_version = "readdir extension: version 2.0";
 #else
 static const char *ext_version = "readdir extension: version 3.0";
-#endif
-
-#include "gawkdirfd.h"
-
-#include "gettext.h"
-#define _(msgid)  gettext(msgid)
-#define N_(msgid) msgid
-
-#ifndef PATH_MAX
-#define PATH_MAX	1024	/* a good guess */
 #endif
 
 static awk_bool_t init_readdir(void);
