365626947d16c74ff08072bfd261f23e50445ba0
Michael M. Builov <mbuilov@gmail.com>
2020-07-12 14:29:15 +0300

    build fix for use mscrtx library

--- gawkmisc.c
+++ gawkmisc.c
@@ -24,6 +24,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  */
 
+#define NO_LOCALE_RPL /* avoid including "mscrtx/localerpl.h" */
 #include "awk.h"
 
 #if defined(HAVE_FCNTL_H)
